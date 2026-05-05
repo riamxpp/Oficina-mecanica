3bf7b05e139293f25b55f0a770b680852a6d235f
José Salustiano <josesalustianojr@gmail.com>
2026-05-05 16:07:54 -0300

    feat: adiciona models clientes

--- backend/clientes/models.py
+++ backend/clientes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    # RN01 - Preenchimento Obrigatório (blank=False, null=False por padrão no Django)
+    nome = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=14, unique=True)
+    data_nascimento = models.DateField()
+    endereco = models.TextField()
+    telefone = models.CharField(max_length=20)
+    
+    # RN06 - Exclusão lógica para manter o histórico
+    is_active = models.BooleanField(default=True) 
+
+    def __str__(self):
+        return self.nome
