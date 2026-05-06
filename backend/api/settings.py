3c657f934a958f30a240849266b2ce400f04902a
Riam Stefesom <riamstefeson616@gmail.com>
2026-05-05 22:38:42 -0300

    fix: adicionando "REST_FRAMEWORK no settings.py

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -41,6 +41,13 @@ INSTALLED_APPS = [
     "rest_framework"
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication', # <--- ESSA LINHA É ESSENCIAL
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
