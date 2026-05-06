9aff6640d7e128e434367ba12f050674281e2e93
Riam Stefesom <riamstefeson616@gmail.com>
2026-05-05 22:48:15 -0300

    feat: criando teste para criação de um novo usuário.

--- backend/user/tests.py
+++ backend/user/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from rest_framework import status
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class UserCRUDTests(APITestCase):
+  def setUp(self):
+    self.user_data = { 
+      'username': 'mecanico_chefe',
+      'email': 'chefe@oficina.com',
+      'password': 'senha_segura_123'
+    }
+    self.user = User.objects.create_user(**self.user_data) # cria um User teste no banco de testes.
+    
+    # Preparando as URLs nomeadas (baseadas no seu urls.py)
+    self.register_url = reverse('register')
+    self.user_detail_url = reverse('user_detail', kwargs={'pk': self.user.pk})
+
+  def test_create_user(self): # cria e registra novo usuário
+    novo_usuario = {
+      'username': 'novo_ajudante',
+      'email': 'ajudante@oficina.com',
+      'password': 'senha_ajudante_123'
+    }
+    
+    response = self.client.post(self.register_url, novo_usuario)
+    
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED) # Verifica se retornou 201 Created
+    self.assertEqual(User.objects.count(), 2) # Agora temos 2 usuários no banco de testes (o criado no setUp e o novo)
