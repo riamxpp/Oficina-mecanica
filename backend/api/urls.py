255925f5b660035a88888c0c5fde540e7dc01225
Riam Stefesom <riamstefeson616@gmail.com>
2026-05-12 15:45:25 -0300

    feat: adicionando o url de core no api.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('clientes.urls')),
     path('api/', include('veiculos.urls')),
+    path('api/', include('core.urls')),
 ]
 
