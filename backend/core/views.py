4fe2ec77cb9fd85afc0010aa96e7db7e13c915c9
Riam Stefesom <riamstefeson616@gmail.com>
2026-05-12 14:22:03 -0300

    feat: criando view para Insumos.

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from backend.core.models import Insumos
+from backend.core.serializers import InsumosSerializer
 
 # Create your views here.
+class InsumosViewSet(viewsets.ModelViewSet):
+  queryset = Insumos.objects.all()
+  serializer_class = InsumosSerializer
+  permission_classes = (IsAuthenticated,)
