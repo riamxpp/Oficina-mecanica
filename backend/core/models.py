50dd1e1875d6985f49e6a5b93a3a553472bd6619
Riam Stefesom <riamstefeson616@gmail.com>
2026-05-12 14:18:58 -0300

    feat: criando modelo para Insumos.

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Insumos(models.Model):
+  nome = models.CharField(max_length=255)
+  marca = models.CharField(max_length=255)
+  descricao = models.TextField()
+  quantidade = models.FloatField(decimal=0.0)
+
+  def __str__(self):
+    return f"{self.nome} - {self.marca}"
