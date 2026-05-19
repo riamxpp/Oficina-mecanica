31ca28af2213a514dc4e6586196d093c45c85257
Ariadny Francisca Dantas Santos <ariadnyfran15@gmail.com>
2026-05-19 10:43:26 -0300

    feat: adicionando models de Ordem de Serviço

--- backend/ordens_servico/models.py
+++ backend/ordens_servico/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from veiculos.models import Veiculo
+from core.models import Procedimento, Insumo
 
-# Create your models here.
+class OrdemServico(models.Model):
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE, related_name='ordens_servico')
+    data_abertura = models.DateTimeField(auto_now_add=True)
+    procedimentos = models.ManyToManyField(Procedimento, blank=True)
+    
+    def __str__(self):
+        return f"OS #{self.id} - {self.veiculo.placa} ({self.get_status_display()})"
