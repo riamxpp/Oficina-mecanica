b00bc4bddf47337d7c82ab7e038ee4d9f150be0c
Ariadny Francisca Dantas Santos <ariadnyfran15@gmail.com>
2026-05-19 11:32:43 -0300

    feat: add metodo de destroy na view de ordem de servico

--- backend/ordens_servico/views.py
+++ backend/ordens_servico/views.py
@@ -68,3 +68,22 @@ class OrdemServicoViewSet(viewsets.ModelViewSet):
             "mensagem": "Dados da Ordem de Serviço atualizados com sucesso!", 
             "dados": serializer.data
         }, status=status.HTTP_200_OK)
+    
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+        except Http404 as e:
+            return Response({"erro": "Erro: A Ordem de Serviço informada não existe."}, status=status.HTTP_404_NOT_FOUND)
+        
+        try:
+            # Em vez de deletar, nós mudamos o status para 'cancelado' e salvamos
+            instance.status = 'cancelado'
+            instance.save()
+            
+            return Response({"mensagem": "A Ordem de Serviço foi cancelada com sucesso!"}, status=status.HTTP_200_OK)
+            
+        except Exception:
+            return Response(
+                {"erro": "Erro: Não foi possível cancelar a Ordem de Serviço devido a uma instabilidade no sistema. Tente novamente."}, 
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
